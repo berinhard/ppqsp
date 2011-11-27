8a9344e1b7997a4588ee2743598f7f5be806d55f
Bernardo Fontes <falecomigo@bernardofontes.net>
2011-11-27 21:46:16 -0200

    Refatora impressão do resultado

--- class_0/for_profiler.py
+++ class_0/for_profiler.py
@@ -22,17 +22,16 @@ def filter_with_filter_func():
 
 
 def create_table_result(title, results):
-    head = 'fn min mean max'.split(' ')
     calc = {
         'fn': title,
         'min': min(results),
         'mean': sum(results) / len(results),
         'max': max(results)
     }
-    sys.stdout.write('{0:>30s} {1:<10s} {2:<10s} {3:<10s}'.format(*head))
-    sys.stdout.write('\n{fn:>30s} {min:<10.8f} {mean:<10.8f} {max:<10.8f}\n'.format(**calc))
+    print '\n{fn:>30s} {min:<10.8f} {mean:<10.8f} {max:<10.8f}\n'.format(**calc)
 
 def profile(stmt='pass', setup='pass'):
+
     t = Timer(stmt, setup)
     r = t.repeat(10, 1000)
 
@@ -41,6 +40,8 @@ def profile(stmt='pass', setup='pass'):
 if __name__ == '__main__':
 
     print "***** FILTER PROFILING *****"
+    head = 'fn min mean max'.split(' ')
+    print '{0:>30s} {1:<10s} {2:<10s} {3:<10s}'.format(*head)
     profile('filter_with_normal_for()', 'from __main__ import filter_with_normal_for')
     profile('filter_with_list_comprehension()', 'from __main__ import filter_with_list_comprehension')
     profile('filter_with_filter_func()', 'from __main__ import filter_with_filter_func')
